68eae30a68851694af6652ea7452bda8349abbd2
FyFean <72227868+FyFean@users.noreply.github.com>
2024-09-08 16:10:35 +0200

    init

--- hw1/test/run_test_coverages.m
+++
@@ -1,4 +0,0 @@
-addpath(fullfile(pwd, '..', 'src'));
-
-rezultatiInterpoliraj = runtests('test', 'ReportCoverageFor', '../src/interpoliraj.m');
-reszultatiZlepek = runtests('test', 'ReportCoverageFor', '../src/Zlepek.m');
--- hw2/test/run_test_coverages.m
+++
@@ -1,6 +0,0 @@
-addpath(fullfile(pwd, '..', 'src'));
-
-rezultatiSi = runtests('test', 'ReportCoverageFor', '../src/aproksimiraj_Si.m');
-rezultatiSimpMetoda = runtests('test', 'ReportCoverageFor', '../src/simp_metoda.m');
-rezultatiPlHipotrohoide = runtests('test', 'ReportCoverageFor', '../src/pl_hipotrohoide.m');
-reszultatiNewtonovaMetoda = runtests('test', 'ReportCoverageFor', '../src/newt_iteracija.m');
